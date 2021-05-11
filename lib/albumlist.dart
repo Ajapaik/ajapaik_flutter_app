2887e95e1987f22626b88be71e8ad1dbe3c6f45e
Kimmo Virtanen <kimmo.virtanen@gmail.com>
2021-05-11 14:04:42 +0300

    changed geojson format and added support for subalbums

--- lib/albumlist.dart
+++ lib/albumlist.dart
@@ -8,29 +8,110 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'camera.dart';
 
-
-class AlbumListPage extends StatelessWidget {
+class AlbumListPage extends StatefulWidget {
   String pageTitle = "Not set";
   String dataSourceUrl = "";
 
   AlbumListPage({Key? key}) : super(key: key);
+
   AlbumListPage.network(this.pageTitle, this.dataSourceUrl);
+
+  @override
+  AlbumListPageState createState() => AlbumListPageState();
+}
+
+class AlbumListPageState extends State<AlbumListPage> {
+  String orderBy = "distance";
+  String orderDirection = "desc";
+  bool searchDialogVisible = false;
+
+  Future<List<Album>>? _albumData;
+
+  void sorting() {
+    setState(() {
+      this.orderBy = (this.orderBy == "distance") ? "alpha" : "distance";
+      refresh();
+    });
+  }
+
+  void refresh() async {
+    await (this._albumData = fetchAlbum(http.Client(), getDataSourceUrl()));
+  }
+
+  void toggleSearchDialog() {
+    this.searchDialogVisible = searchDialogVisible ? false : true;
+  }
+
+  String getDataSourceUrl() {
+    String url = widget.dataSourceUrl;
+    if (url.contains("?")) {
+      url +=
+          "&orderby=" + this.orderBy + "&orderdirection=" + this.orderDirection;
+    } else {
+      url +=
+          "?orderby=" + this.orderBy + "&orderdirection=" + this.orderDirection;
+    }
+    return url;
+  }
+
+  @override
+  void initState() {
+    refresh();
+
+    super.initState();
+  }
+
+  Future<List<Album>> test(BuildContext context) {
+    return this._albumData!;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(pageTitle),
+        title: Text(widget.pageTitle),
+        actions: <Widget>[
+/*          IconButton(
+              icon: Icon(Icons.search),
+              tooltip: "Search",
+              onPressed: toggleSearchDialog),*/
+          IconButton(
+              icon: Icon(((this.orderBy == "distance")
+                  ? Icons.sort_sharp
+                  : Icons.sort_by_alpha)),
+              tooltip: "Sort",
+              onPressed: sorting),
+        ],
       ),
-      body: FutureBuilder<List<Album>>(
-        future: fetchAlbum(http.Client(), dataSourceUrl),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) print(snapshot.error);
+      body: Column(children: [
+        Flexible(
+            child: FutureBuilder<List<Album>>(
+          future: test(context),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) print(snapshot.error);
 
-          return snapshot.hasData
-              ? AlbumList(albums: snapshot.data)
-              : Center(child: CircularProgressIndicator());
-        },
+            return (snapshot.hasData)
+                ? AlbumList(albums: snapshot.data)
+                : Center(child: CircularProgressIndicator());
+          },
+        )),
+      ]),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {},
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.photo_library),
+            label: 'Nearest',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_a_photo),
+            label: 'Add a photo',
+          ),
+        ],
       ),
     );
   }
@@ -39,7 +120,6 @@ class AlbumListPage extends StatelessWidget {
 class AlbumList extends StatelessWidget {
   final List<Album>? albums;
 
-
   AlbumList({Key? key, this.albums}) : super(key: key);
 
   void _take_rephoto(context, index) {
@@ -47,33 +127,79 @@ class AlbumList extends StatelessWidget {
       CameraDescription firstCamera = availableCameras.first;
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) =>
-            TakePictureScreen(
-                camera: firstCamera, historicalPhotoUri: albums![index].properties.thumbnail.toString())),
+        MaterialPageRoute(
+            builder: (context) => TakePictureScreen(
+                camera: firstCamera,
+                historicalPhotoUri: albums!
+                    .first.features[index].properties.thumbnail
+                    .toString())),
       );
     });
+  }
+
+  void _move_to_geojson(context, index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => AlbumListPage.network(
+              albums!.first.features[index].properties.name!,
+              albums!.first.features[index].properties.geojson!)),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return StaggeredGridView.countBuilder(
-      crossAxisCount: 4,
-      itemCount: albums!.length,
-      staggeredTileBuilder: (int index) => new StaggeredTile.fit(2),
-      mainAxisSpacing: 4.0,
-      crossAxisSpacing: 4.0,
-      itemBuilder: (context, index) {
-        return new GestureDetector(
-            onTap: () {_take_rephoto(context, index); },
-            child: Column(children: [
-              CachedNetworkImage(
-                  imageUrl: albums![index].properties.thumbnail.toString()),
-              Text(
-                albums![index].properties.name.toString(),
-                textAlign: TextAlign.center,
-              )
-            ]));
-      },
-    );
+        crossAxisCount: 4,
+        staggeredTileBuilder: (int index) =>
+            new StaggeredTile.fit(index == 0 ? 4 : 2),
+        mainAxisSpacing: 4.0,
+        crossAxisSpacing: 4.0,
+        itemCount: albums!.first.features.length + 1,
+        // Number of items + header row
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return headerTile(context, index);
+          } else {
+            return contentTile(context, index);
+          }
+        });
+  }
+
+  Widget headerTile(context, index) {
+    var headerImage;
+
+    if (albums!.first.image != null && albums!.first.image != "") {
+      headerImage = CachedNetworkImage(imageUrl: albums!.first.image);
+    } else {
+      headerImage = Container();
+    }
+
+    return Center(
+        child:
+            Column(children: [headerImage, Text(albums!.first.description)]));
+  }
+
+  Widget contentTile(context, index) {
+    // Remove header row from index
+    index = index - 1;
+    return new GestureDetector(
+        onTap: () {
+          if (albums!.first.features[index].properties.geojson != null &&
+              albums!.first.features[index].properties.geojson != "") {
+            _move_to_geojson(context, index);
+          } else {
+            _take_rephoto(context, index);
+          }
+        },
+        child: Column(children: [
+          CachedNetworkImage(
+              imageUrl: albums!.first.features[index].properties.thumbnail
+                  .toString()),
+          Text(
+            albums!.first.features[index].properties.name.toString(),
+            textAlign: TextAlign.center,
+          )
+        ]));
   }
 }
--- lib/data/album.geojson.dart
+++ lib/data/album.geojson.dart
@@ -3,46 +3,110 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart';
 
 class Album {
+  List<Feature> features;
+  String name = "";
+  String description = "";
+  String image = "";
+
+  Album(
+      {required this.name,
+      required this.description,
+      required this.image,
+      required this.features});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    String name = (json['name'] != null) ? json['name'].toString() : "";
+    String description =
+        (json['description'] != null) ? json['description'].toString() : "";
+    String image = (json['image'] != null) ? json['image'].toString() : "";
+
+    List<Feature> featureList = (json['features'] != null)
+        ? json['features']
+            .map<Feature>((i) => Feature.fromJson(i) as Feature)
+            .toList()
+        : Feature.empty();
+    return Album(
+        name: name,
+        description: description,
+        image: image,
+        features: featureList);
+  }
+}
+
+class Feature {
   String type = "";
-  Geometry? geometry;
-  late Properties properties;
+  Geometry geometry;
+  Properties properties;
 
-  Album({required this.type, this.geometry, required this.properties});
+  Feature(
+      {required this.type, required this.geometry, required this.properties});
 
-  Album.fromJson(Map<String, dynamic> json) {
-    type = json['type'];
+  factory Feature.empty() {
+    return Feature(
+        type: "", geometry: Geometry.empty(), properties: Properties.empty());
+  }
 
-    geometry = (json['geometry'] != null && json['geometry'].toString() != "[]")
-        ? new Geometry.fromJson(json['geometry'])
-        : null;
-    properties = (json['properties'] != null)
+  factory Feature.fromJson2(Map<String, dynamic> json) {
+    String type2 = json['type'];
+    Properties properties2 = (json['properties'] != null)
         ? new Properties.fromJson(json['properties'])
         : new Properties.empty();
+
+    print(json["geometry"].toString());
+    Geometry geometry2 =
+        ((json['geometry'] != null) && (json['geometry'].toString() != "[]"))
+            ? new Geometry.fromJson(json['geometry'])
+            : new Geometry.empty();
+
+    return new Feature(
+        type: type2, geometry: geometry2, properties: properties2);
+  }
+
+  factory Feature.fromJson(Map<String, dynamic> json) {
+    String type2 = json['type'];
+
+    Geometry geometry2 =
+        ((json['geometry'] != null) && (json['geometry'].toString() != "[]"))
+            ? new Geometry.fromJson(json['geometry'])
+            : new Geometry.empty();
+
+    Properties properties2 = (json['properties'] != null)
+        ? new Properties.fromJson(json['properties'])
+        : new Properties.empty();
+
+    return Feature(type: type2, geometry: geometry2, properties: properties2);
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['type'] = this.type;
     if (this.geometry != null) {
-      data['geometry'] = this.geometry!.toJson();
+      data['geometry'] = this.geometry.toJson();
     }
     if (this.properties != null) {
       data['properties'] = this.properties.toJson();
     }
-
     return data;
   }
 }
 
 class Geometry {
-  String type = "";
-  List<String> coordinates = [];
+  String type;
+  List<String> coordinates;
 
   Geometry({required this.type, required this.coordinates});
 
-  Geometry.fromJson(Map<String, dynamic> json) {
-    type = (json['type'] != null) ? json['type'] : "";
-    coordinates = json['coordinates'].cast<String>();
+  factory Geometry.fromJson(Map<String, dynamic> json) {
+/*    String type = json['type'];
+    //List<String> coordinates = new List<String>.from(json['coordinates']);
+
+    List<String> coordinates = [];
+*/
+    return Geometry(type: "", coordinates: []);
+  }
+
+  factory Geometry.empty() {
+    return Geometry(type: "", coordinates: []);
   }
 
   Map<String, dynamic> toJson() {
@@ -64,18 +128,21 @@ class Properties {
   String? favorites;
   int? rephotos;
   String? thumbnail;
+  String? geojson;
 
-  Properties(
-      {required this.id,
-      required this.name,
-      required this.description,
-      required this.date,
-      required this.author,
-      required this.sourceUrl,
-      required this.sourceLabel,
-      required this.favorites,
-      required this.rephotos,
-      required this.thumbnail});
+  Properties({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.date,
+    required this.author,
+    required this.sourceUrl,
+    required this.sourceLabel,
+    required this.favorites,
+    required this.rephotos,
+    required this.thumbnail,
+    required this.geojson,
+  });
 
   Properties.fromJson(Map<String, dynamic> json) {
     id = json['id'].toString();
@@ -87,6 +154,7 @@ class Properties {
     sourceLabel = json['source_label'];
     favorites = json['favorites'].toString();
     rephotos = json['rephotos'];
+    geojson = json['geojson'];
     thumbnail =
         json['thumbnail'].toString().replaceFirst("http://", "https://");
   }
@@ -102,6 +170,7 @@ class Properties {
     favorites = "";
     rephotos = 0;
     thumbnail = "";
+    geojson = "";
   }
 
   Map<String, dynamic> toJson() {
@@ -117,6 +186,7 @@ class Properties {
     data['rephotos'] = this.rephotos;
     // Only https is allowed
     data['thumbnail'] = this.thumbnail;
+    data['geojson'] = this.geojson;
     return data;
   }
 }
