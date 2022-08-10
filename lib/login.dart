6c0ada0379edeaeb83d32b1f74670d251dd4e105
Ilkka Prusi <ilkka.prusi@gmail.com>
2022-08-10 10:18:35 +0300

    comments

--- lib/login.dart
+++ lib/login.dart
@@ -69,6 +69,7 @@ class DisplayLoginScreen extends StatelessWidget {
       ),*/
 
     // TODO: if user wants to upload to social media, possibly other different auth methods as well?
+    // -> not only different login provider, but also different server (commons, ajapaik)
 
     return Center(
         child: Wrap(spacing: 10, runSpacing: 10, children: buttons
