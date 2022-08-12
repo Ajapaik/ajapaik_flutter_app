506481ce795af0b3be637b3f0c8a186018787432
Ilkka Prusi <ilkka.prusi@gmail.com>
2022-08-12 09:32:49 +0300

    simplified

--- lib/data/album.geojson.dart
+++ lib/data/album.geojson.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-import 'package:ajapaik_flutter_app/services/geolocation.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart';
 
